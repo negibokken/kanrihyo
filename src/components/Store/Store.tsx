bb6e896ad9164b7e81733a7f36bb1d7c2a01e21c
negibokken <negibokken@gmail.com>
2017-08-22 06:20:42 +0900

    WIP: Add comment to make store

--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -5,3 +5,26 @@ export function reducer(state: any, action: any): any {
 }
 
 export const store: any = createStore(reducer, {todo: []});
+
+// http://developers.mobage.jp/blog/redux-basic
+// 1. make action (type)
+//     function addToDo(text) {
+//        return {
+//          type: 'ADD_TODO',
+//          id: nextTodoId++,
+//          text: text
+//        }
+//     }
+// 2. make reducer
+//    function reducer(state, action) {
+//      switch(action.type) {
+//        case 'ADD_TODO':
+//          return {
+//            todo: state.todo.concat({
+//              id: action.id,
+//              text: action.text,
+//              completed: false,
+//            })
+//        };
+//      }
+//    }
